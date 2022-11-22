ade401884d42e812587bcd375d11e90378187997
Fernando Carranza <foreverfas_fc@hotmail.com>
2022-11-21 21:09:41 -0600

    :bug: solved windows get info login

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -12,13 +12,13 @@ class Student extends Model
 
     public function checkLogin($code, $password, $ip)
     {
-        $validate = DB::connection('sqlsrv')->select('EXEC ValidarAlumno ?,?,?', [$code, $password, $ip]);
+        $validate = DB::connection('sqlsrv')->select('SET NOCOUNT ON;EXEC ValidarAlumno ?,?,?', [$code, $password, $ip]);
         return $validate[0]->valor;
     }
 
     public function getInformation($code)
     {
-        $data = DB::connection('sqlsrv')->select('EXEC PPAGOS_OBTIENE_DATOS_ESTUDIANTE ?', [$code]);
+        $data = DB::connection('sqlsrv')->select('SET NOCOUNT ON;EXEC PPAGOS_OBTIENE_DATOS_ESTUDIANTE ?', [$code]);
         return $data[0];
     }
 }
