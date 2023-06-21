6b1a8c2c78cb89599e6773213a405f812f5db60c
Fernando Carranza <foreverfas_fc@hotmail.com>
2023-06-21 17:39:29 -0600

    :bug: checking ECI code response

--- app/Helpers/PaymentHelper.php
+++ app/Helpers/PaymentHelper.php
@@ -60,3 +60,47 @@ function checkAuthentication($authentication)
             break;
     }
 }
+
+function checkISO($iso)
+{
+    switch ($iso) {
+        case '03':
+            return "Comercio Inválido";
+            break;
+        case '05':
+            return "Transacción Declinada";
+            break;
+        case '12':
+            return "Transacción Inválida";
+            break;
+        case '57':
+            return "Tipo de tarjeta inváliido";
+            break;
+        case '89':
+            return "Autenticación Fracasó";
+            break;
+        case '91':
+            return "Se agotó el tiempo de respuesta";
+            break;
+        case '96':
+        case '98':
+        case '99':
+            return "Error del sistema";
+            break;
+        case '97':
+            return "Fracasó la solicitud de validación";
+            break;
+        case 'HP0':
+            return "Prepocesamiento finalizado en la Página Alojadas";
+            break;
+        case 'SP4':
+            return "Prepocesamiento SPI completado";
+            break;
+        case 'TK0':
+            return "Tokenización completada";
+            break;
+        default:
+            return 'No se pudo aplicar 3DS a la transacción';
+            break;
+    }
+}
